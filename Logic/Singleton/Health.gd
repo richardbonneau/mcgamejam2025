e8df2ad26146019e9f0b7550f1387d61891fc576
Jean-CharlesMarentette <jean-charles.marentette.1@ens.etsmtl.ca>
2025-01-10 20:28:43 -0500

    Update Health.gd

--- Logic/Singleton/Health.gd
+++ Logic/Singleton/Health.gd
@@ -1,11 +1,28 @@
 extends Node
 
+signal TakeDamage(player, damage)
+signal HealDamage(player, damage)
+signal PlayerDied(player)
+
+const MAX_HEALTH = 100;
+
+var playerHealth  = [MAX_HEALTH, MAX_HEALTH];
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	TakeDamage.connect(on_player_damaged)
+	HealDamage.connect(on_player_healed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func on_player_damaged(playerIndex: int, damageTaken: int):
+	playerHealth[playerIndex] -= damageTaken;
+	if (playerHealth[playerIndex] <= 0):
+		PlayerDied.emit(playerIndex);
+		
+func on_player_healed(playerIndex: int, damageHealed: int):
+	playerHealth[playerIndex] += damageHealed
+	if (playerHealth[playerIndex] > MAX_HEALTH):
+		playerHealth[playerIndex] = MAX_HEALTH
