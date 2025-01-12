9bee22a1457262e7161116f0ebfb96e19f79dcdf
richardbonneau <richardbonneau91@gmail.com>
2025-01-12 14:20:12 -0500

    breakable crates

--- Prototype/Jimmy/Assets/crate.gd
+++ Prototype/Jimmy/Assets/crate.gd
@@ -1,2 +1,12 @@
 extends StaticBody3D
 class_name Crate
+
+var times_hit = 0
+
+func hit():
+	times_hit+=1
+	if times_hit == 1:
+		$CrateFull.hide()
+		$BrokenCrate.show()
+	elif times_hit > 1:
+		queue_free()
