16d86f4264f46b7e24d9dd29d4f0023bc1f29037
Jean-CharlesMarentette <jean-charles.marentette.1@ens.etsmtl.ca>
2025-01-12 13:15:35 -0500

    quit button in main menu

--- Logic/UI/MainMenu.gd
+++ Logic/UI/MainMenu.gd
@@ -13,3 +13,6 @@ func _process(delta: float) -> void:
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Logic/Main/MainRichard.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
